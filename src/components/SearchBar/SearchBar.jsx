77d544021a137c881f6f45b20eaa572041fbab37
Dawyid22 <119014306+Dawyid22@users.noreply.github.com>
2023-07-01 16:04:20 -0300

    "modificado"

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,8 +1,9 @@
+import search from "./SearchBar.module.css"
 export default function SearchBar(props) {
    const {searchHandler} = props
    return (
-      <div>
-         <input type='search' />
+      <div className={search.searchButton}>
+         <input className={search.inputColor} type='search' />
          <button onClick={searchHandler}>Agregar</button>
       </div>
    );
