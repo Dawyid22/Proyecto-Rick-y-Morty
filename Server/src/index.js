6556d8a777e1eb9d5a05910ab01dc892ee2f0e93
Dawyid22 <119014306+Dawyid22@users.noreply.github.com>
2023-07-28 11:37:11 -0300

    "ultimo intento de arreglar el login"

--- Server/src/index.js
+++ Server/src/index.js
@@ -3,8 +3,7 @@ const server = express();
 const { conn } = require("./DB_connection");
 const PORT = 3001;
 
-conn.sync({ force: true }).then(() => {
-  server.listen(PORT, () => {
-    console.log(`Server raised in port: ${PORT}`);
-  });
+server.listen(PORT, () => {
+  conn.sync({ force: true });
+  console.log(`Server raised in port: ${PORT}`);
 });
